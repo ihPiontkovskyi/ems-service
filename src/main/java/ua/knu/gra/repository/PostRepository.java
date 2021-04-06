7dfce54cd98725e296da2cd99f48b6154eb4e5b0
Piontkovskyi, Ihor <Ihor.Piontkovskyi@McKesson.com>
2021-04-06 20:36:04 +0300

    [GRA-0000] Add tests

--- src/main/java/ua/knu/gra/repository/PostRepository.java
+++ src/main/java/ua/knu/gra/repository/PostRepository.java
@@ -9,10 +9,10 @@ import java.util.Optional;
 
 @Repository
 public interface PostRepository extends JpaRepository<PostModel, Integer> {
-    @Query(value = "select * from posts p join courses c on p.courses_id = c.id where p.sequence_number = ?1 and c.uid = ?2", nativeQuery = true)
+    @Query(value = "select * from posts p join courses c on p.course_id = c.id where p.sequence_number = ?1 and c.uid = ?2", nativeQuery = true)
     Optional<PostModel> findBySeqNumbAndCourseUid(Integer seqNum, String uid);
 
-    @Query(value = "select * from posts p join groups g on p.group_id = g.id where p.sequence_number = ?1 and g.uid = ?2", nativeQuery = true)
+    @Query(value = "select * from posts p join user_groups g on p.group_id = g.id where p.sequence_number = ?1 and g.uid = ?2", nativeQuery = true)
     Optional<PostModel> findBySeqNumbAndGroupUid(Integer seqNum, String uid);
 
 }
